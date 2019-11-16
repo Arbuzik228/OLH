709ac31a0bcaae4a656d74a6ae2509d6211f343c
Arbuik228 <detgo6587@gmail.com>
2019-11-16 13:32:45 +0200

    GOOOOD WORKKKKKKK

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        boolean as=true;
+        boolean boo=true;
+        System.out.println(""+ !(as||boo) + (!as&&!boo) );
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        int hoop=666666665;
+        while (hoop<666666666) {
+            System.out.println("cat eat chicken");
+            hoop++;
+        }
+        for (int tgh=0;tgh<hoop;tgh+=666666666){
+            System.out.println(tgh);
+        }
+        int food=666666665;
+        int fod=666666666;
+        do  {
+            System.out.println("/////////////////////////////");
+            food++;
+        }while (food<=fod);
+
+    }
+}
